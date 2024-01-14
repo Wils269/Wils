1f4771ed48134ed97c42c6b3b5e30a64318b03ca
Vyacheslav Potapov <WilsWilsov@gmail.com>
2024-01-14 20:16:24 +0300

    Первый урок

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = 'первая переменная';
+let screens = 'вторая переменная';
+let screenPrice = 'третяя переменная';
+let rollback = 'четвертая переменная';
+let fullPrice = 'пятая переменная';
+let adaptive = 'шестая переменная';
+
+alert("Внимание! Внимание!")
+console.log("Приветствую Вас!)");
