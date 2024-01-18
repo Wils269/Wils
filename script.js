f1dd56c0a5796b1c2e7f6249968a14aa1625c648
Vyacheslav Potapov <WilsWilsov@gmail.com>
2024-01-19 00:12:31 +0300

    Выполненное первое усложненное задание

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let title = 'первая переменная';
-let screens = 'вторая переменная';
-let screenPrice = 'третяя переменная';
-let rollback = 'четвертая переменная';
-let fullPrice = 'пятая переменная';
-let adaptive = 'шестая переменная';
+let num = 266219,
+    sum,
+    degree;
 
-alert("Внимание! Внимание!")
-console.log("Приветствую Вас!)");
+sum = 2 * 6 * 6 * 2 * 1 * 9;
+console.log(sum);
+
+degree = sum ** 3
+console.log(degree);
+
+console.log(String(degree).slice(0,2));
