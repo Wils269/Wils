77950474de5bf63945925114f4eed8be4d0ef080
Vyacheslav Potapov <WilsWilsov@gmail.com>
2024-01-22 00:54:55 +0300

    Выполнил третее задание

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
-let title = 'firstPeoject';
-let screens = "Простые, Cложные, Bнтерактивные";
-let screenPrice = 88;
+'use strict';
+
+let title = prompt("Как называется ваш проект?");
+console.log(title);
+
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Cложные, Интерактивные");
+console.log(screens);
+
+let screenPrice = +prompt("Сколько будет стоить данная работа?", 12000);
+
 let rollback = 13;
-let fullPrice = 150000;
-let adaptive = true;
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
+let adaptive = prompt("Нужен ли адаптив на сайте?");
 
-console.log(screens.length);
+if (adaptive == "Да") {
+    console.log(true);
+} else if (adaptive == "да") {
+    console.log(true);
+} else {
+    console.log(false);
+}
 
-console.log(screenPrice+'$');
-console.log(fullPrice+'$');
+let service1 = prompt ("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt ("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
-console.log(screens.toLocaleLowerCase().split(","));
+let servicePercentPrice = fullPrice - fullPrice * rollback / 100;
 
-console.log(fullPrice*(rollback/100));
+console.log(fullPrice);
+console.log(Math.ceil(servicePercentPrice));
+
+switch (true) {
+    case fullPrice > 30000:
+            console.log("Даем скидку в 10%");
+        break;
+    case fullPrice > 15000 && fullPrice < 30000:
+            console.log("Даем скидку в 5%");
+        break;
+    case fullPrice < 15000 && fullPrice > 0:
+            console.log("Скидка не предусмотрена");
+        break;
+    case fullPrice <= 0:
+            console.log ("Что-то пошло не так")
+};
